b8aa6355a1925c0dff7d966f6f6ab6b4c91ac909
Jairo <jairo.garcia@ceu.es>
2022-09-27 19:57:19 +0200

    Ejercicio 1 de arrays

--- ejercicios-propuestos/arrays/index.php
+++ ejercicios-propuestos/arrays/index.php
@@ -11,6 +11,18 @@ $meses = [1 => 'Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto'
 $dias = [1 => 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 $anyo = [];
 
+//Recorro el array de meses
+foreach ($meses as $numMes => $nombreMes){
+    //Recorro el array con los días que tiene ese mes
+    for ($i = 1; $i <= $dias[$numMes]; $i++){
+        $anyo[$numMes][$i] = $i." de ".$nombreMes;
+    }
+}
+
+/*$array = [];
+for ($i = 0; $i < 500; $i++){
+    $array[$i] = $i;
+}*/
 ?>
 
 <html lang="es">
@@ -26,5 +38,8 @@ $anyo = [];
             cada elemento en la forma $array[6][15] y un echo devuelva "15 de Junio".
         </p>
         <hr>
+        <p>
+            <?php echo "Mi cumpleaños es el ". $anyo[6][15] ?>
+        </p>
     </body>
 </html>
